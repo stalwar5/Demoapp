8ad23d8b1efb74bab3a8f9d8055fbd91df3d7e0c
Srishtti Talwar <srishttitalwar@Srishttis-MacBook-Pro.local>
2019-01-09 11:09:00 +0530

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Srishtti Talwar on 1/9/19.
+//  Copyright © 2019 Srishtti Talwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
